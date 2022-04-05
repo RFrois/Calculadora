9e0b916c655d5b5edd9ee4437bb0ba5b59723e31
RFrois <99776659+RFrois@users.noreply.github.com>
2022-04-04 22:01:29 -0300

    apresentado para o professor em sala de aula

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
 import os
-from crypt import methods
 from flask import Flask, jsonify, request, abort, redirect, url_for, render_template
 from math import sqrt
-from CalculadoraBasica import CalculadoraBasica 
 
 app  = Flask(__name__)
 
-@app.route('/calc') 
+@app.route("/")
+def main():
+    return render_template('calc.html')
 
+@app.route('/calc', methods=['GET', 'POST']) 
 def calc():
-    if methods == "POST":
-        valor1=request.form.get("v1")
-        valor2=request.form.get("v2")
-        operacao=request.form.get("op")
+    valor1 = request.form['v1']
+    valor2 = request.form['v2']
+    operacao = request.form['op']
 
-        v1 = int(valor1)
-        v2 = int(valor2)
-        op = str(operacao)
+    v1 = int(valor1)
+    v2 = int(valor2)
+    op = str(operacao)
 
-        rc = CalculadoraBasica()
+    if (op == 'soma'):
+        rc = (float(v1) + float(v2))
+    elif (op == 'divisao'):
+        rc = (float(v1) / float(v2))
+    elif (op == 'subtracao'):
+        rc = (float(v1) - float(v2))
+    elif (op == 'multiplicacao'):
+        rc = (float(v1) * float(v2))
+    else:
+        rc = 0
+    return(str(rc))
 
-        if (op == "soma"):
-            rc = (float(v1) + float(v2))
-        elif (op == "divisao"):
-            rc = (float(v1) / float(v2))
-        elif (op == "subtracao"):
-            rc = (float(v1) - float(v2))
-        elif (op == "multiplicacao"):
-            rc = (float(v1) * float(v2))
-        else:
-            print("errouuuu")
-        return(rc)
-    #calc()
 if __name__ == ("__main__"):
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
